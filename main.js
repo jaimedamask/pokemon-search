4d7ce2e59728518cf16a5fd4a4573e5af4830f61
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-08-07 20:37:50 -0400

    Feat: Add function to retrieve and display Pokémon info

--- main.js
+++ main.js
@@ -12,3 +12,30 @@ const defense = document.getElementById('defense');
 const specialAttack = document.getElementById('special-attack');
 const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
+
+const fetchPokemon = async () => {
+    try {
+        const nameOrId = searchInput.value.toLowerCase();
+        const res = await fetch(
+            `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${nameOrId}`
+        );
+        const data = await res.json();
+
+        pokemonName.textContent = `${data.name.toUpperCase()}`;
+        pokemonId.textContent = `#${data.id}`;
+        weight.textContent = `Weight: ${data.weight}`;
+        height.textContent = `Height: ${data.height}`;
+        spriteContainer.innerHTML = `
+            <img id='sprite' src='${data.sprites.front_default}' alt='${data.name} front default sprite'>
+        `;
+
+        hp.textContent = data.stats[0].base_stat;
+        attack.textContent = data.stats[1].base_stat;
+        defense.textContent = data.stats[2].base_stat;
+        specialAttack.textContent = data.stats[3].base_stat;
+        specialDefense.textContent = data.stats[4].base_stat;
+        speed.textContent = data.stats[5].base_stat;
+    } catch (err) {
+        alert('Pokémon not found');
+    }
+};
